58476d2f4229b162e350ea28b3b137a251853c45
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-08-19 13:27:25 +0300

    feat: create express-server and connect mongo db

---
+++ backend/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import productRoutes from './routes/product';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const { DB_ADDRESS } = process.env;
+
+if (!DB_ADDRESS) {
+  throw new Error('❌ DB_ADDRESS is not defined in .env');
+}
+
+mongoose.connect(DB_ADDRESS)
+  .then(() => {
+    console.log('✅ Успешное подключение к MongoDB');
+  })
+  .catch((err) => {
+    console.error('❌ Ошибка подключения к MongoDB:', err);
+  });
+
+app.use('/', productRoutes);
+
+app.listen(3000, () => { console.log('Server started port 3000'); });
---
+++ backend/src/controllers/products.ts
@@ -0,0 +1,35 @@
+import {Request, Response} from 'express';
+import Product from '../models/product';
+
+// GET /product — получить все товары
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: 'Ошибка при получении товаров', error: err });
+  }
+};
+
+// POST /product — создать новый товар
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { title, image, category, description, price } = req.body;
+
+    const newProduct = new Product({
+      title,
+      image,
+      category,
+      description,
+      price,
+    });
+
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (err: any) {
+    if (err.code === 11000) { // дубликат уникального поля title
+      return res.status(400).json({ message: 'Товар с таким названием уже существует' });
+    }
+    res.status(500).json({ message: 'Ошибка при создании товара', error: err });
+  }
+};
